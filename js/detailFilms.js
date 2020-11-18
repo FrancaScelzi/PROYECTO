948fa6f0190a85d453adcf14d019910951fd5b90
FrancaScelzi <69602652+FrancaScelzi@users.noreply.github.com>
2020-11-18 19:45:05 -0300

    Varios ajustes 18/11

--- js/detailFilms.js
+++ js/detailFilms.js
@@ -2,26 +2,26 @@
 
 let queryString = location.search;
 
-let queryObject = new URLSearchParams (queryString);
-console.log (queryObject);
+let queryObject = new URLSearchParams(queryString);
+console.log(queryObject);
 
 let id = queryObject.get('id');
-console.log (id)
+console.log(id)
 
 let apiKey = "50a53e8e9d1beeefe2442f1dbc53288d";
 
 let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
 
-let containerDeatil = document.querySelector ('main');
+let containerDeatil = document.querySelector('main');
 
-fetch (`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
-.then (datos=>datos.json() )
-.then (respuesta => {
-    console.log (respuesta);
+fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+  .then(datos => datos.json())
+  .then(respuesta => {
+    console.log(respuesta);
     let detail = ''
-    
-        detail += 
-        `<div class="contenido">
+
+    detail +=
+      `<div class="contenido">
         <img class="imagen" src="https://image.tmdb.org/t/p/w500/${respuesta.poster_path}" alt="">
         <div>
         <h3 class="titulo">${respuesta.title}</h3>
@@ -31,26 +31,58 @@ fetch (`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
         
         <li><strong>Promedio de votos</strong>: ${respuesta.vote_average} </li>
         <li><strong>Género</strong>: ${respuesta.genres[0].name} </li>
-        <li><strong>Reviews</strong>: </li>
+        <li class="reviews"><strong>Reviews</strong>: </li>
         </ul>
         </div>
         
         </div>`
-      
-      
 
-      containerDeatil.innerHTML= detail
+
+
+    containerDeatil.innerHTML = detail
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${apiKey}`)
+      .then(datos => datos.json())
+      .then(respuesta => {
+        console.log(respuesta)
+
+
+        let containerReviews = document.querySelector ('.reviews')
+
+        let resultados = respuesta.results;
+
+    
+
+          
+          respuesta.results.forEach(index => {
+
+         
+              
+              let content = index.content ;
+              
+              if (content.length > 300) {
+                let reviewPart = content.slice(0,300);
+                content = reviewPart + '...';
+              }
+              
+              
+              containerReviews.innerHTML += `${content} `
+            
+            
+            
+          });
         
-    })
+        
+        
 
-// fetch (`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${apiKey}`)
-// .then (datos=>datos.json() )
-// .then (respuesta => {
-//     console.log (respuesta)
-// let detail = ''
+        
 
-// detail +=
-// // `PONER REVIEWS`
+      })
 
-// })
-  
+
+
+      
+
+
+
+  })
--- js/searchResults.js
+++ js/searchResults.js
@@ -5,12 +5,13 @@ let queryString = location.search;
 let queryObject = new URLSearchParams (queryString);
 console.log (queryObject);
 
-let search = queryObject.get('search');
+let search = queryObject.get('searchData');
 console.log (search)
 
 let apiKey = "50a53e8e9d1beeefe2442f1dbc53288d";
 
 let url = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${search}&page=1&include_adult=false`
+
 let containerResults = document.querySelector ('main');
 
 fetch (url)
@@ -19,33 +20,46 @@ fetch (url)
     console.log (respuesta);
     let results = ''
 
+
      respuesta.results.forEach ((multi, index) => {
        if (multi.media_type == "tv"){
          results += 
-         ` <section class="contenido">
-         <a href="detailFilms.html?id=${multi.id}"> 
+         ` <section>
+         <a href="detailSeries.html?id=${multi.id}"> 
            <article class= "polaroid" > 
                <img class = "imagen" src= "https://image.tmdb.org/t/p/w500/${multi.poster_path}"> 
                <div class = "textopolaroid"> 
                    <p class= "textopolaroidtitulo"> ${multi.name}</p> 
                    <p class = "textopolaroidaño"> ${multi.first_air_date} </p>
+                </div>
             </article>
         </a>
-       </section>` }
+       </section>` 
+
+       containerResults.innerHTML= results
+      
+      }
 
       else if (multi.media_type == "movie"){
         results +=
-        ` <section class="contenido">
+        ` <section>
          <a href="detailFilms.html?id=${multi.id}"> 
            <article class= "polaroid" > 
                <img class = "imagen" src= "https://image.tmdb.org/t/p/w500/${multi.poster_path}"> 
                <div class = "textopolaroid"> 
                    <p class= "textopolaroidtitulo"> ${multi.title}</p> 
                    <p class = "textopolaroidaño"> ${multi.release_date} </p>
+                </div>
             </article>
         </a>
        </section>` 
+
+       containerResults.innerHTML= results
       }
-      containerResults.innerHTML= results
+
+      
        })
+
+    // .catch(error => console.error (error))
+      
       })
